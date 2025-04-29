71e4b814200248cc68fcc8e4dec6d836d89116c4
Obedaya <yeneuro.mail@gmail.com>
2025-04-29 08:17:39 +0200

    New config changes

--- ubuntu-script.sh
+++ ubuntu-script.sh
@@ -21,10 +21,10 @@ sudo ufw default deny outgoing
 sudo ufw allow from 192.168.56.0/24 to any port 80
 sudo ufw allow out 80/tcp
 sudo ufw allow out 443/tcp
-sudo ufw allow out 22
-sudo ufw allow out 2222
-sudo ufw allow from 192.168.56.0/24 to any port 2222
-sudo ufw allow from 192.168.56.0/24 to any port 22
+sudo ufw allow out 22/tcp
+sudo ufw allow out 2222/tcp
+sudo ufw allow 2222/tcp
+sudo ufw allow 22/tcp
 
 echo "y" | sudo ufw enable
 
